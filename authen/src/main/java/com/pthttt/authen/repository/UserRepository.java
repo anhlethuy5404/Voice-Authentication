86d6c85b2869d4ce7e1f28822307d4df465bb67a
bang <candyyeukoi10@gmail.com>
2025-11-02 20:46:10 +0700

    ...

--- authen/src/main/java/com/pthttt/authen/repository/UserRepository.java
+++ authen/src/main/java/com/pthttt/authen/repository/UserRepository.java
@@ -9,7 +9,6 @@ import com.pthttt.authen.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
     User findByEmail(String email);
     List<User> findByRole(String role);
 }
